e6f7d94e5ed17d93dc27cfd48816bc3fe00ccb30
Kirill <kirya_gla@mail.ru>
2025-03-18 15:54:04 +0300

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        question: "Which device is required for the Internet connection?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans:4,
+    },
+    {
+        question: "First page of Website is termed as?",
+        option1:"Index Page",
+        option2:"Home Page",
+        option3:"Sitemap",
+        option4:"Pen Drive",
+        ans:2,
+    },
+]
+
+
+
+
+
+
+
+
